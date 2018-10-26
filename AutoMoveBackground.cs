1f5d91ff3f859d5039cafd32e387b6e987b8d1af
지수 김 <rhlcp23@naver.com>
2018-10-26 20:52:27 +0900

    FirstTest

---
+++ AutoMoveBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoveBackground : MonoBehaviour {
+
+    [SerializeField] private Transform Other;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.left * Time.deltaTime * 0.1f);
+
+        if(transform.position.x < -4.65f)
+        {
+            transform.position = new Vector3(transform.position.x + 9.3f, transform.position.y, transform.position.z);
+        }
+
+	}
+}
---
+++ DictionarySelecter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DictionarySelecter : MonoBehaviour {
+
+    [SerializeField] private GameObject Monsters;
+    [SerializeField] private GameObject Selecter;
+    [SerializeField] private int MonsterNumber;
+
+    private Button button;
+
+    public void Select()
+    {
+        for(int i = 0; i< Monsters.transform.childCount; i++)
+        {
+            if (Monsters.transform.GetChild(i).gameObject.activeSelf)
+                Monsters.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        Selecter.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        button = GetComponent<Button>();
+
+        if (UQGameManager.Instance.Monsters[MonsterNumber])
+        {
+            button.interactable = true;
+            transform.GetChild(0).gameObject.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
+        else
+        {
+            button.interactable = false;
+            transform.GetChild(0).gameObject.GetComponent<Image>().color = new Color32(0, 0, 0, 255);
+        }
+    }
+}
